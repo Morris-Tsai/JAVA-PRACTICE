6bb69dbf2531c40291fb9852cf7001f10467c495
User <User@192.168.36.28>
2019-06-21 16:00:12 +0800

    Overloading & Overriding Practice

---
+++ Mix/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   Animal c =new Animal();
+	   c.eat();
+       Animal a =new Horse();
+       a.eat();
+       Horse b =new Horse();
+       b.eat("shit");
+	}
+
+}
