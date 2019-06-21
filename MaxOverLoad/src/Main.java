b41351f6f1f7065d67db7a15967790702440a7b3
User <User@192.168.36.28>
2019-06-21 15:46:39 +0800

    Overloading Practice

---
+++ MaxOverLoad/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=10 ,b=5;
+     double c=10.2,d=10.3;
+     MaxOverLoad m =new  MaxOverLoad();
+     System.out.println("The MAX number(5,10) is "+m.max(a, b));
+     System.out.println("The MAX number(10.2,10.3) is "+m.max(c, d));
+	}
+
+}
---
+++ MaxOverLoad/src/MaxOverLoad.java
@@ -0,0 +1,10 @@
+
+public class MaxOverLoad {
+	public int max(int x,int y) {
+   	 return x>y?x:y;
+    }
+	public double max(double x,double y) {
+	   	 return x>y?x:y;
+	}
+	
+}
